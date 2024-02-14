cf51f85d5b7f455d45745a4b4d8e156512c3879a
Echo <journeyjersey@outlook.com>
2024-02-14 21:47:07 +0000

    slight readability changes

--- Back/file_management.py
+++ Back/file_management.py
@@ -3,6 +3,7 @@ def add_url(dir: str, url: str) -> str:
         lines = [line.strip() for line in file.readlines()]
 
     try:
+
         if url not in lines:
             # If not, append it
             lines.append(url)
@@ -13,8 +14,10 @@ def add_url(dir: str, url: str) -> str:
                     file.write(line + '\n')
             print(f"URL {url} appended to {dir}")
             return f"Successfully added RSS feed"
+        
         else:
             print(f"URL {url} already present in {dir}")
             return f"Already present RSS feed"
+        
     except UnboundLocalError:
         return f"Unbound Local Error in function add_url({dir},{url})\n(Back/file_management.py)"
