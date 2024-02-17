53342567e3a6d343f9f5e71b05b079daebd2de35
Echo <journeyjersey@outlook.com>
2024-02-17 13:02:59 +0000

    fixed CSS

--- front/socket_stuff.py
+++ front/socket_stuff.py
@@ -65,21 +65,23 @@ def handle_request(client_socket) -> None:
                 user_feeds_dir="Back/user_feeds.txt",
                 user_choices_dir="Back/user_choices.txt"
                 )
-            with open("Front/style.css","r") as style:
+            with open("Front/style.css","r") as f:
+                style = f.read()
                 response_data = f"<head><style>{style}</style></head><body><h1>Feeds</h1><p>{feeds}</p></body>"
                 print(style)
 
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(response_data)}\r\n\r\n{response_data}"
         else:
             response = f"HTTP/1.1 500 Internal Server Error"
+            
     
     client_socket.sendall(response.encode())
-    print(response.encode())
+    #print(response.encode())
     client_socket.close()
 
 def format_data(form_input: str) -> str:
     form_input = form_input.split("=", 1)  # Splits from the first '=', outputting https%3A%2F%2Ffeeds.megaphone.fm%2Fnewheights
-    form_input = form_input[1].strip().replace("%3A", ":").replace("%2F", "/").replace("%2f", "/")
+    form_input = form_input[1].strip().replace("%3A", ":").replace("%2F", "/").replace("%2f", "/") #Todo: fix this behaviour
     #print(f"After: {form_input}")
     return form_input
 
