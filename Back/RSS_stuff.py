d2d0cf8bf1164bb679be878583cd79b45be6324d
Echo <journeyjersey@outlook.com>
2024-03-06 19:55:38 +0000

    Changed to dicts, functions the same

--- Back/RSS_stuff.py
+++ Back/RSS_stuff.py
@@ -3,22 +3,31 @@ import aiohttp
 import asyncio
 from file_management import handle_error
 
-async def fetch_feed(session, url):
+async def fetch_feed(session, feed_name, url):
     try:
         async with session.get(url) as response:
             return await response.text()
     except aiohttp.client_exceptions.InvalidURL:
         print(f"Invalid URL {url}")
         handle_error(f"Invalid URL {url}")
+    except aiohttp.client_exceptions.ClientConnectorError:
+        print(f"URL {url} could not connect. This error has not been logged.")
 
-async def parse_url(user_feeds_dir: str, user_choices_dir: str):
+async def parse_url(user_feeds_dir: str, user_choices_dir: str): 
+#TODO: split the getting of URLs into:
+    #Get URLs from a CSV file, as to allow titles of the feeds
+    #Make results a dictionary with the title and then the feed
+    
+
+
     user_choices = get_choices(user_choices_dir)
     user_feeds = get_feeds(user_feeds_dir)
+    print(user_feeds)
     results = []
 
     async with aiohttp.ClientSession() as session:
-        for url in user_feeds:
-            feed_content = await fetch_feed(session, url)
+        for feed_name, url in user_feeds.items():
+            feed_content = await fetch_feed(session, feed_name, url)
             try:
                 root = ET.fromstring(feed_content)
                 feed_items = []
@@ -54,7 +63,7 @@ async def parse_url(user_feeds_dir: str, user_choices_dir: str):
                     temp_result = f"<a href='{item[link_index]}'>{item[title_index]}</a>"
                 except IndexError:
                     temp_result = "URL broken!"
-                    handle_error("""Broken URL caused broken response "URL broken!" This is due to an IndexError in Back/RSS_stuff.py. Please check your feeds in user_feeds.txt for any broken feeds.""")
+                    handle_error("""Broken URL caused broken response "URL broken!" This is due to an IndexError in Back/RSS_stuff.py. Please check your feeds in user_feeds.csv for any broken feeds.""")
             else:
                 temp_result = ""
             for choice in user_choices:
@@ -78,14 +87,26 @@ def get_choices(dir: str):
         user_choices = [choice.strip() for choice in user_choices]
     return user_choices 
 
-def get_feeds(dir: str):
+def get_feeds(dir: str) -> dict:
     with open(dir,"r") as f:
-        user_feeds = f.readlines()
-        user_feeds = [feed.strip() for feed in user_feeds]
+        list_user_feeds: list = f.readlines()
+        list_user_feeds = [feed.strip() for feed in list_user_feeds] 
+        #List ["BBC UK,http://feeds.bbci.co.uk/news/uk/rss.xml", 
+        #"Sky News UK,http://feeds.skynews.com/feeds/rss/uk.xml]"
+        
+        user_feeds: dict = {}
+        
+        for feed_info in list_user_feeds:
+            name, url = feed_info.split(",")
+            user_feeds[name.strip()] = url.strip()
+            #Dict {"BBC UK":"http://feeds.bbci.co.uk/news/uk/rss.xml", 
+            #"Sky News UK":"http://feeds.skynews.com/feeds/rss/uk.xml"}
+
+
     return user_feeds 
 
 if __name__ == "__main__":
     asyncio.run(parse_url(
-        user_feeds_dir="Back/user_feeds.txt",
+        user_feeds_dir="Back/user_feeds.csv",
         user_choices_dir="Back/user_choices.txt"
     ))
