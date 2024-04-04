4c05a876a5d62b7a625667c3a0bbf54f48c79989
Echo <journeyjersey@outlook.com>
2024-04-05 00:56:31 +0100

    closer to PEP-8

--- Back/RSS_stuff.py
+++ Back/RSS_stuff.py
@@ -3,17 +3,19 @@ import aiohttp
 import asyncio
 from file_management import handle_error
 
+
 async def fetch_feed(session, feed_name, url):
     try:
         async with session.get(url) as response:
             response_text = await response.text()
             return response_text
-        
+
     except aiohttp.client_exceptions.InvalidURL:
         print(f"Invalid URL {url}")
         handle_error(f"Invalid URL {url}")
     except aiohttp.client_exceptions.ClientConnectorError:
         print(f"URL {url} could not connect. This error has not been logged.")
+
 
 async def fetch_all_feeds(session, user_feeds):
     tasks = []
@@ -25,32 +27,32 @@ async def fetch_all_feeds(session, user_feeds):
         print(f"Error fetching feeds: {e}")
         return []
 
-async def parse_url(session, user_feeds_dir: str, user_choices_dir: str): 
-#TODO: split the getting of URLs into:
-    #Get URLs from a CSV file, as to allow titles of the feeds
-    #Make results a dictionary with the title and then the feed
-    
 
+async def parse_url(session, user_feeds_dir: str, user_choices_dir: str):
+    # TODO: split the getting of URLs into:
+    # Get URLs from a CSV file, as to allow titles of the feeds
+    # Make results a dictionary with the title and then the feed
 
     user_choices = get_choices(user_choices_dir)
     user_feeds = get_feeds(user_feeds_dir)
-    user_feeds = await fetch_all_feeds(session, user_feeds=user_feeds) #returns a list of feeds.
+    # returns a list of feeds.
+    user_feeds = await fetch_all_feeds(session, user_feeds=user_feeds)
     results = []
     for feed_content in user_feeds:
         try:
             root = ET.fromstring(feed_content)
             feed_items = []
             items = root.findall(".//item")
-            print(items)
+            # print(items)
             for item in items:
                 current_item = []
                 for choice in user_choices:
                     found_element = item.find(choice)
                     if found_element is not None:
                         choice_text = found_element.text
-                        current_item.append(choice_text)                     
+                        current_item.append(choice_text)
                     else:
-                        #print(f"Could not find {choice} in {item}")
+                        # print(f"Could not find {choice} in {item}")
                         pass
 
                 feed_items.append(current_item)
@@ -75,45 +77,51 @@ async def parse_url(session, user_feeds_dir: str, user_choices_dir: str):
                     temp_result = f"<a href='{item[link_index]}'>{item[title_index]}</a>"
                 except IndexError:
                     temp_result = "URL broken!"
-                    handle_error("""Broken URL caused broken response "URL broken!" This is due to an IndexError in Back/RSS_stuff.py. Please check your feeds in user_feeds.csv for any broken feeds.""")
+                    handle_error(
+                        """Broken URL caused broken response "URL broken!" This is due to an IndexError in Back/RSS_stuff.py. Please check your feeds in user_feeds.csv for any broken feeds.""")
             else:
                 temp_result = ""
             for choice in user_choices:
-                if ((choice not in ["title", "link"]) or (has_link == True and has_title == False and choice == "link") or (has_link == False and has_title == True and  choice == "title")):
+                if ((choice not in ["title", "link"]) or (has_link == True and has_title == False and choice == "link") or (has_link == False and has_title == True and choice == "title")):
                     try:
                         temp_result += f"<br>{item[user_choices.index(choice)]}"
                     except:
                         pass
             inner_result.append(temp_result)
-            inner_result.append("<br>")
+            inner_result.append("<br>")  # <br> <THIS br> <br>
+        # <THIS BR> <br> <THIS BR>
         final_results.append("<br>".join(inner_result))
+        # Between feeds, only 1 <br> tag appears. This sucks because 2 are meant to appear.
 
     results = "\n".join(final_results)
 #   with open("Back/results.txt","w") as f:
 #       f.write(str(results))
     return results
 
+
 def get_choices(dir: str):
-    with open(dir,"r") as f:
+    with open(dir, "r") as f:
         user_choices = f.readlines()
         user_choices = [choice.strip() for choice in user_choices]
-    return user_choices 
+    return user_choices
+
 
 def get_feeds(dir: str) -> dict:
-    with open(dir,"r") as f:
+    with open(dir, "r") as f:
         list_user_feeds: list = f.readlines()
-        list_user_feeds = [feed.strip() for feed in list_user_feeds] 
-        #List ["BBC UK,http://feeds.bbci.co.uk/news/uk/rss.xml", 
-        #"Sky News UK,http://feeds.skynews.com/feeds/rss/uk.xml]"
-        
+        list_user_feeds = [feed.strip() for feed in list_user_feeds]
+        # List ["BBC UK,http://feeds.bbci.co.uk/news/uk/rss.xml",
+        # "Sky News UK,http://feeds.skynews.com/feeds/rss/uk.xml]"
+
         user_feeds: dict = {}
-        
+
         try:
             for feed_info in list_user_feeds:
                 name, url = feed_info.split(",")
                 user_feeds[name.strip()] = url.strip()
-                #Dict {"BBC UK":"http://feeds.bbci.co.uk/news/uk/rss.xml", 
-                #"Sky News UK":"http://feeds.skynews.com/feeds/rss/uk.xml"}
+                # Dict {"BBC UK":"http://feeds.bbci.co.uk/news/uk/rss.xml",
+                # "Sky News UK":"http://feeds.skynews.com/feeds/rss/uk.xml"}
         except ValueError:
-            handle_error("There's a problem in your Back/user_feeds.csv, where you have either forgotten the title, URL, or comma. Please check your error.")
-    return user_feeds 
+            handle_error(
+                "There's a problem in your Back/user_feeds.csv, where you have either forgotten the title, URL, or comma. Please check your error.")
+    return user_feeds
